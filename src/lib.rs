403ba493af7622e5316d57e4e40798c28c87683b
sigpher <425223341@qq.com>
2025-12-09 21:51:05 +0800

    batch insert users

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
+
+pub fn demo() {
+    let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    for id in 0..100 {
+        let builder = thread::Builder::new().name(id.to_string());
+        let counter_clone = Arc::clone(&counter);
+        let handle = builder
+            .spawn(move || {
+                let mut num = counter_clone.lock().unwrap();
+                println!("thread {}", thread::current().name().unwrap());
+                *num += 1;
+            })
+            .unwrap();
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("Result: {}", *counter.lock().unwrap());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,73 @@
-use std::{
-    sync::{Arc, Mutex},
-    thread,
-};
+use std::{fs, str::FromStr, time::Duration};
 
-fn main() {
-    let counter = Arc::new(Mutex::new(0));
+use sqlx::{SqlitePool, error::Error, prelude::FromRow, sqlite::SqliteConnectOptions};
 
-    let mut handles = vec![];
+#[derive(Debug, FromRow)]
+struct User {
+    // id: i64,
+    name: String,
+    age: Option<i32>,
+    email: String,
+}
 
-    for id in 0..100 {
-        let builder = thread::Builder::new().name(id.to_string());
-        let counter_clone = Arc::clone(&counter);
-        let handle = builder
-            .spawn(move || {
-                let mut num = counter_clone.lock().unwrap();
-                println!("thread {}", thread::current().name().unwrap());
-                *num += 1;
-            })
-            .unwrap();
-        handles.push(handle);
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let options = SqliteConnectOptions::from_str("fmc.sqlite")?
+        .create_if_missing(true)
+        .journal_mode(sqlx::sqlite::SqliteJournalMode::Wal)
+        .synchronous(sqlx::sqlite::SqliteSynchronous::Normal)
+        .busy_timeout(Duration::from_secs(5));
+    let pool = SqlitePool::connect_with(options).await?;
+
+    let create_db_sql = fs::read_to_string("sql/create_user.sql").unwrap();
+    sqlx::query(&create_db_sql).execute(&pool).await?;
+
+    batch_insert_users(&pool).await?;
+
+    Ok(())
+}
+
+async fn batch_insert_users(pool: &SqlitePool) -> Result<(), Error> {
+    let mut tx = pool.begin().await?;
+
+    let users = vec![
+        User {
+            // id: 1,
+            name: "choi".into(),
+            age: Some(30),
+            email: "cwy@tdyh.com.cn".into(),
+        },
+        User {
+            // id: 2,
+            name: "lora".into(),
+            age: Some(26),
+            email: "zdq@tdyh.com.cn".into(),
+        },
+        User {
+            // id: 3,
+            name: "troy".into(),
+            age: Some(6),
+            email: "cly@tdyh.com.cn".into(),
+        },
+        User {
+            // id: 4,
+            name: "elan".into(),
+            age: Some(2),
+            email: "cyl@tdyh.com.cn".into(),
+        },
+    ];
+
+    for user in users.iter() {
+        sqlx::query("INSERT OR IGNORE INTO users (name, age, email) VALUES(?, ?, ?)")
+            .bind(&user.name)
+            .bind(&user.age)
+            .bind(&user.email)
+            .execute(&mut *tx)
+            .await?;
     }
 
-    for handle in handles {
-        handle.join().unwrap();
-    }
-    println!("Result: {}", *counter.lock().unwrap());
+    tx.commit().await?;
+    sqlx::query("VACUUM;").execute(pool).await?;
+
+    Ok(())
 }
